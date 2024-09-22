47d6fa95e5e0b0bd74fa3141a83c57ec2d4af4af
maher <mnstudios@hotmail.com>
2024-09-22 19:08:11 +0100

    add health check

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/health")
+async def health_check():
+    return {"detail": "ok"}
