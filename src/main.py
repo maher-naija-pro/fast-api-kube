bcce27815a3bb9fc54305993b39ad41cf1627205
maher <mnstudios@hotmail.com>
2024-09-24 12:12:27 +0100

    clean health file

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
+import os 
+import time
+import sys
+sys.path.append("routers")
+
 from fastapi import FastAPI
+from routers import health
+from routers import metric
 
 app = FastAPI()
-
+app.include_router(health.router, prefix="")
+app.include_router(metric.router, prefix="")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    is_k8s = "KUBERNETES_SERVICE_HOST" in os.environ
+    return {
+        "version": "0.1.0",
+        "date": int(time.time()),
+        "kubernetes": is_k8s
+    }
 
-@app.get("/health")
-async def health_check():
-    return {"detail": "ok"}
