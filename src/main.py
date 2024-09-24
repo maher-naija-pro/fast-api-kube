eebdeac27ff0fb76066cfa61eb22337b599fdab7
maher <mnstudios@hotmail.com>
2024-09-24 12:54:10 +0100

    add ip validation

--- src/main.py
+++ src/main.py
@@ -7,10 +7,13 @@ sys.path.append("routers")
 from fastapi import FastAPI
 from routers import health
 from routers import metric
+from routers import tools
 
 app = FastAPI()
 app.include_router(health.router, prefix="")
 app.include_router(metric.router, prefix="")
+app.include_router(tools.router, prefix="/v1")
+
 
 @app.get("/")
 async def root():
@@ -21,3 +24,6 @@ async def root():
         "kubernetes": is_k8s
     }
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=3000)
