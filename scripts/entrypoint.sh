3cc274a6fa4a3f29d13b0c4d0b897a570466e8b0
maher <mnstudios@hotmail.com>
2024-09-25 15:00:22 +0100

    update timout

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 alembic upgrade head
-hypercorn src/main:app -b 0.0.0.0:3000 --reload  --access-logfile -  --graceful-timeout 5  
-ga 
+hypercorn src/main:app -b 0.0.0.0:3000 --reload  --access-logfile -  --graceful-timeout 0  
