253dabca7470695a5b5bb71939e4a0aaf285f042
maher <mnstudios@hotmail.com>
2024-09-25 13:26:17 +0100

    add logs to tools

--- src/routers/tools.py
+++ src/routers/tools.py
@@ -1,21 +1,23 @@
-from fastapi import APIRouter,Request,HTTPException,Depends
-from pydantic import BaseModel
-from db.database import get_db
-from sqlalchemy.orm import Session
-from db.database import get_db
-import ipaddress
-import socket
 import sys
-
-router= APIRouter(prefix='/tools')
 sys.path = ["", ".."] + sys.path[1:]
 sys.path.append("src")
 
 from src.models.log import QueryLog
+from fastapi import APIRouter,Request,HTTPException,Depends
+from pydantic import BaseModel
+from db.database import get_db
+from helpers.log.logger import init_log
+from sqlalchemy.orm import Session
+from db.database import get_db
+import ipaddress
+import socket
+
+
+router= APIRouter(prefix='/tools')
+logger=init_log()
 
 class IPSchema(BaseModel):
     ip: str
-
 
 # Helper function to log successful domain queries
 def log_query( domain: str,db: Session= Depends(get_db),):
