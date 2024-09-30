f890ffbf6d6e84bedb37bac4bec53a16e0d0abc0
maher <mnstudios@hotmail.com>
2024-09-30 21:23:29 +0100

    fix python package setup

--- setup.py
+++ setup.py
@@ -6,27 +6,13 @@ This module uses setuptools to configure the distribution package for the
 version, description, author details, and licensing information. Additionally,
 it includes the required dependencies and package details for distribution.
 
-Functions:
-    readme(): Reads and returns the contents of the README.md file.
 """
 
 from setuptools import setup
 
-
-def readme():
-    """
-    Read and return the contents of the README.md file.
-
-    Returns:
-        str: The contents of the README.md file as a string.
-    """
-    with open("README.md", encoding="utf-8") as f:
-        return f.read()
-
-
 setup(
     name="fast-api-kube",
-    version="0.1",
+    version="0.2",
     description="fast-api-kube",
     url="https://github.com/maher-naija-pro/fast-api-kube",
     author="maher naija",
